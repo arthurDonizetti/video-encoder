dac12f2ec7d68bd92778f8e27fba9a1da50f3fb1
Arthur Donizetti <arthur.donizetti@gmail.com>
2024-06-19 00:03:56 -0300

    Feature/validate entities (#12)
    * validate video
    
    * include job validation

--- encoder/internal/domain/job.go
+++ encoder/internal/domain/job.go
@@ -1,13 +1,45 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	uuid "github.com/satori/go.uuid"
+)
 
 type Job struct {
-	ID               string
-	OutputBucketPath string
-	Status           string
+	ID               string `validate:"uuid"`
+	OutputBucketPath string `validate:"required"`
+	Status           string `validate:"required"`
 	Video            *Video
+	VideoID          string
 	Error            string
 	CreatedAt        time.Time
 	UpdatedAt        time.Time
+}
+
+func NewJob(output, status string, video *Video) (*Job,error) {
+	j := &Job{OutputBucketPath: output, Status: status, Video: video, VideoID: video.ID}
+	j.prepare()
+	
+	err := j.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return j, nil
+}
+
+func (j *Job) prepare() {
+	j.ID = uuid.NewV4().String()
+	j.CreatedAt = time.Now()
+	j.UpdatedAt = time.Now()
+}
+
+func (j *Job) Validate() error {
+	validator := validator.New()
+
+	err := validator.Struct(j)
+
+	return err
 }
--- encoder/internal/domain/video.go
+++ encoder/internal/domain/video.go
@@ -1,10 +1,31 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
 
 type Video struct {
-	ID         string
-	ResourceID string
-	FilePath   string
+	ID         string `validate:"uuid"`
+	ResourceID string `validate:"required"`
+	FilePath   string `validate:"required"`
 	CreatedAt  time.Time
+}
+
+func NewVideo() *Video {
+	return &Video{
+		ID:         "",
+		ResourceID: "",
+		FilePath:   "",
+		CreatedAt:  time.Time{},
+	}
+}
+
+func (v *Video) Validate() error {
+	validator := validator.New()
+
+	err := validator.Struct(v)
+
+	return err
 }
